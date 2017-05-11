cf7d50d5c68f6338da41e11ddea9434501307118
Samuel Loup <loup@epsitec.ch>
2017-05-11 08:14:59 +0200

    Unwire lazy loading demo

--- widgets/hello.js
+++ widgets/hello.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default () => {
   return  (
     <div>
-      <h1>Hello!</h1>
+      <h1>Hello from Laboratory!</h1>
     </div>
   );
 };
--- widgets/loading.js
+++ widgets/loading.js
@@ -4,7 +4,7 @@ export default function Loading({isLoading, pastDelay, error}) {
   if (isLoading && pastDelay) {
     return <p>Loading...</p>;
   } else if (error && !isLoading) {
-    return <p>Error! {error}</p>;
+    return <p>{error.toString ()}</p>;
   } else {
     return null;
   }
