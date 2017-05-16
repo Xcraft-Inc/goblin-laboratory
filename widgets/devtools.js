3a576698f737a49d3c3662a268045a9dca8b6483
Mathieu Schroeter <mathieu@schroetersa.ch>
2017-05-16 13:28:26 +0200

    Add the immutable-devtools package

---
+++ widgets/devtools.js
@@ -0,0 +1,7 @@
+// FIXME: only with development run
+
+import Perf from 'react-addons-perf';
+window.Perf = Perf;
+
+const installDevTools = require ('immutable-devtools');
+installDevTools (require ('immutable'));
