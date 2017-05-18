64d44e1f0e59f56882df83a95ba11b538c993c4c
Samuel Loup <loup@epsitec.ch>
2017-05-18 06:59:37 +0200

    Prepare redux-form in laboratory

--- widgets/store/root-reducer.js
+++ widgets/store/root-reducer.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import {routerReducer} from 'react-router-redux';
+import {reducer as formReducer} from 'redux-form';
 import backendReducer from 'laboratory/store/backend-reducer';
 
 export default combineReducers ({
   backend: backendReducer,
   routing: routerReducer,
+  form: formReducer,
 });
