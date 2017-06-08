2a43ce4aafc17bc3b8dcb567c712fdfedf9fa18b
Samuel Loup <loup@epsitec.ch>
2017-06-08 14:40:06 +0200

    Work on first layout with Container

--- widgets/laboratory.js
+++ widgets/laboratory.js
@@ -2,7 +2,10 @@ import React from 'react';
 import Widget from 'laboratory/widget';
 import {ConnectedRouter} from 'react-router-redux';
 import {Route} from 'react-router';
+import Container from 'gadgets/container';
+import Button from 'gadgets/button';
 import importer from './importer';
+
 const viewImporter = importer ('view');
 
 class Laboratory extends Widget {
@@ -24,22 +27,34 @@ class Laboratory extends Widget {
         return {path: v, component: k};
       });
       return (
-        <section>
-          <span>{id}</span>
-          <ConnectedRouter history={history}>
-            <div>
-              {routes.map ((route, i) => {
-                return (
-                  <Route
-                    key={i}
-                    path={route.path}
-                    component={viewImporter (route.component)}
-                  />
-                );
-              })}
-            </div>
-          </ConnectedRouter>
-        </section>
+        <ConnectedRouter history={history}>
+          <Container kind="root">
+            <Container kind="left-bar">
+              <Container kind="task-bar">
+                <Button
+                  text-transform="none"
+                  tooltip="Changer de mandat"
+                  kind="task-logo"
+                />
+              </Container>
+            </Container>
+            <Container kind="right">
+              <Container kind="content">
+                <Container kind="top-bar" />
+                {routes.map ((route, i) => {
+                  return (
+                    <Route
+                      key={i}
+                      path={route.path}
+                      component={viewImporter (route.component)}
+                    />
+                  );
+                })}
+              </Container>
+              <span>{id}</span>
+            </Container>
+          </Container>
+        </ConnectedRouter>
       );
     };
   }
