fee1f36d0a30d7467ebeca8e27d5f3dfbf8fd538
Mathieu Schroeter <mathieu@schroetersa.ch>
2017-06-16 14:41:35 +0200

    Add comment about why-did-you-update bug

--- widgets/devtools.js
+++ widgets/devtools.js
@@ -5,6 +5,7 @@ const installDevTools = require ('immutable-devtools');
 installDevTools (require ('immutable'));
 
 /* Look for unnecessary updates (it's expensive), uncomment only for testing
+ * Seems not working properly with PureComponent, https://github.com/garbles/why-did-you-update/issues/28
 
 import React from 'react';
 import {whyDidYouUpdate} from 'why-did-you-update';
