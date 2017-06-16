2cd67d05dbf7206b20f25f040cc808e3f1c18a95
Mathieu Schroeter <mathieu@schroetersa.ch>
2017-06-16 09:28:53 +0200

    Add why-did-you-update module (commented)
    You will see a lot of potential useless rerenders...

--- widgets/devtools.js
+++ widgets/devtools.js
@@ -3,3 +3,19 @@ window.Perf = Perf;
 
 const installDevTools = require ('immutable-devtools');
 installDevTools (require ('immutable'));
+
+/* Look for unnecessary updates (it's expensive), uncomment only for testing
+
+import React from 'react';
+import {whyDidYouUpdate} from 'why-did-you-update';
+
+let {createClass} = React;
+Object.defineProperty (React, 'createClass', {
+  set: nextCreateClass => {
+    createClass = nextCreateClass;
+  },
+});
+
+whyDidYouUpdate (React);
+
+*/
