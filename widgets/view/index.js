eaf773a60477e0d3fb69437dd33b543924e33c4d
Samuel Loup <loup@epsitec.ch>
2017-06-20 10:21:49 +0200

    Impl. new widget for views

---
+++ widgets/view/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Widget from 'laboratory/widget';
+
+class View extends Widget {
+  constructor (props) {
+    super (props);
+  }
+
+  render () {
+    return <div>Missing view render implementation</div>;
+  }
+}
+
+export default View;
