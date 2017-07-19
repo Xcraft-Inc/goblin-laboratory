30d2cc096a8ec3ba042731991aee89b132449f8a
Mathieu Schroeter <schroeter@epsitec.ch>
2017-07-19 14:51:35 +0200

    Simplify code by using the existing doAs method

--- widgets/widget/index.js
+++ widgets/widget/index.js
@@ -200,14 +200,7 @@ class Widget extends React.PureComponent {
   }
 
   do (action, args) {
-    if (!this.props.id) {
-      console.error (`${this.name} is not a connected widget (need an id)`);
-      return;
-    }
-    this.cmd (
-      `${this.name}.${action}`,
-      Object.assign ({id: this.props.id}, args)
-    );
+    return this.doAs (this.name, action, args);
   }
 
   doAs (service, action, args) {
