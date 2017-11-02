9af1acfb59d00c6a4cc0f2688d137ba35b31150e
Samuel Loup <loup@epsitec.ch>
2017-11-02 13:43:00 +0100

    Remove react-perf import for react-16

--- widgets/devtools.js
+++ widgets/devtools.js
@@ -1,5 +1,5 @@
-import Perf from 'react-addons-perf';
-window.Perf = Perf;
+//import Perf from 'react-addons-perf';
+//window.Perf = Perf;
 
 const installDevTools = require ('immutable-devtools');
 installDevTools (require ('immutable'));
