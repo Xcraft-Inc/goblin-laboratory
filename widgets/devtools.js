737afb0b3d01de0d2b8cbb6a923ca79e48a0eda4
Mathieu Schroeter <mathieu@schroetersa.ch>
2018-03-19 07:23:47 +0100

    Remove dead module

--- widgets/devtools.js
+++ widgets/devtools.js
@@ -3,20 +3,3 @@
 
 const installDevTools = require('immutable-devtools');
 installDevTools(require('immutable'));
-
-/* Look for unnecessary updates (it's expensive), uncomment only for testing
- * Seems not working properly with PureComponent, https://github.com/garbles/why-did-you-update/issues/28
-
-import React from 'react';
-import {whyDidYouUpdate} from 'why-did-you-update';
-
-let {createClass} = React;
-Object.defineProperty (React, 'createClass', {
-  set: nextCreateClass => {
-    createClass = nextCreateClass;
-  },
-});
-
-whyDidYouUpdate (React);
-
-*/
