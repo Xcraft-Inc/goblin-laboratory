c1fd99536372a502ba21e49a3cb9bacbdd884322
Mathieu Schroeter <mathieu@schroetersa.ch>
2018-05-31 13:49:42 +0200

    Add more font awesome icons

--- widgets/laboratory/fa.js
+++ widgets/laboratory/fa.js
@@ -1,13 +1,21 @@
+// TODO: add support for free version (for the open-source version of westeros)
 import fontawesome from '@fortawesome/fontawesome';
 
-// TODO: add support for free version (for the open-source version of westeros)
+/* A */
 import faAngleDown from '@fortawesome/fontawesome-pro-solid/faAngleDown';
 import faAngleUp from '@fortawesome/fontawesome-pro-solid/faAngleUp';
 import faArchive from '@fortawesome/fontawesome-pro-solid/faArchive';
 import faArrowRight from '@fortawesome/fontawesome-pro-solid/faArrowRight';
 import faArrowAltCircleLeft from '@fortawesome/fontawesome-pro-solid/faArrowAltCircleLeft';
+import faAsterisk from '@fortawesome/fontawesome-pro-solid/faAsterisk';
+
+/* B */
+import faBars from '@fortawesome/fontawesome-pro-solid/faBars';
+import faBalanceScale from '@fortawesome/fontawesome-pro-solid/faBalanceScale';
 import faBell from '@fortawesome/fontawesome-pro-solid/faBell';
 import faBicycle from '@fortawesome/fontawesome-pro-solid/faBicycle';
+
+/* C */
 import faCalendarPlus from '@fortawesome/fontawesome-pro-solid/faCalendarPlus';
 import faCar from '@fortawesome/fontawesome-pro-solid/faCar';
 import faCaretDown from '@fortawesome/fontawesome-pro-solid/faCaretDown';
@@ -15,46 +23,91 @@ import faCartPlus from '@fortawesome/fontawesome-pro-solid/faCartPlus';
 import faCheck from '@fortawesome/fontawesome-pro-solid/faCheck';
 import faCheckSquare from '@fortawesome/fontawesome-pro-solid/faCheckSquare';
 import faChild from '@fortawesome/fontawesome-pro-solid/faChild';
-import faClone from '@fortawesome/fontawesome-pro-solid/faClone';
+import faCircle from '@fortawesome/fontawesome-pro-regular/faCircle';
+import faClock from '@fortawesome/fontawesome-pro-solid/faClock';
+import faClone from '@fortawesome/fontawesome-pro-regular/faClone';
+import faColumns from '@fortawesome/fontawesome-pro-solid/faColumns';
 import faCube from '@fortawesome/fontawesome-pro-light/faCube';
+
+/* D */
+import faDotCircle from '@fortawesome/fontawesome-pro-regular/faDotCircle';
+
+/* E */
+import faEllipsisH from '@fortawesome/fontawesome-pro-solid/faEllipsisH';
 import faEraser from '@fortawesome/fontawesome-pro-solid/faEraser';
+import faExclamationTriangle from '@fortawesome/fontawesome-pro-solid/faExclamationTriangle';
 import faEye from '@fortawesome/fontawesome-pro-solid/faEye';
+import faEyeSlash from '@fortawesome/fontawesome-pro-solid/faEyeSlash';
+
+/* F */
 import faFemale from '@fortawesome/fontawesome-pro-solid/faFemale';
 import faFileAlt from '@fortawesome/fontawesome-pro-solid/faFileAlt';
 import faFlask from '@fortawesome/fontawesome-pro-solid/faFlask';
+
+/* H */
+import faHashtag from '@fortawesome/fontawesome-pro-solid/faHashtag';
+
+/* I */
 import faIndustry from '@fortawesome/fontawesome-pro-solid/faIndustry';
+
+/* L */
 import faLanguage from '@fortawesome/fontawesome-pro-solid/faLanguage';
+import faLink from '@fortawesome/fontawesome-pro-solid/faLink';
+
+/* M */
 import faMale from '@fortawesome/fontawesome-pro-solid/faMale';
+import faMapMarkerAlt from '@fortawesome/fontawesome-pro-solid/faMapMarkerAlt';
 import faMinus from '@fortawesome/fontawesome-pro-solid/faMinus';
 import faMinusSquare from '@fortawesome/fontawesome-pro-solid/faMinusSquare';
+import faMoneyBillAlt from '@fortawesome/fontawesome-pro-solid/faMoneyBillAlt';
+
+/* P */
 import faPencil from '@fortawesome/fontawesome-pro-solid/faPencil';
 import faPlus from '@fortawesome/fontawesome-pro-solid/faPlus';
 import faPlusSquare from '@fortawesome/fontawesome-pro-solid/faPlusSquare';
+
+/* R */
 import faRocket from '@fortawesome/fontawesome-pro-solid/faRocket';
+
+/* S */
 import faSearch from '@fortawesome/fontawesome-pro-solid/faSearch';
 import faSignInAlt from '@fortawesome/fontawesome-pro-solid/faSignInAlt';
 import faSmile from '@fortawesome/fontawesome-pro-solid/faSmile';
 import faSpinner from '@fortawesome/fontawesome-pro-solid/faSpinner';
 import faSquare from '@fortawesome/fontawesome-pro-regular/faSquare';
+
+/* T */
 import faTimes from '@fortawesome/fontawesome-pro-solid/faTimes';
 import faTint from '@fortawesome/fontawesome-pro-solid/faTint';
 import faToggleOff from '@fortawesome/fontawesome-pro-light/faToggleOff';
 import faTrain from '@fortawesome/fontawesome-pro-solid/faTrain';
+import faTrash from '@fortawesome/fontawesome-pro-solid/faTrash';
 import faTruck from '@fortawesome/fontawesome-pro-solid/faTruck';
 import faTv from '@fortawesome/fontawesome-pro-solid/faTv';
+
+/* U */
 import faUndo from '@fortawesome/fontawesome-pro-solid/faUndo';
+import faUnlink from '@fortawesome/fontawesome-pro-solid/faUnlink';
 import faUser from '@fortawesome/fontawesome-pro-solid/faUser';
 import faUserSecret from '@fortawesome/fontawesome-pro-solid/faUserSecret';
 
 function boot() {
   fontawesome.library.add(
+    /* A */
     faAngleDown,
     faAngleUp,
     faArchive,
     faArrowRight,
     faArrowAltCircleLeft,
+    faAsterisk,
+
+    /* B */
+    faBars,
+    faBalanceScale,
     faBell,
     faBicycle,
+
+    /* C */
     faCalendarPlus,
     faCar,
     faCaretDown,
@@ -62,34 +115,71 @@ function boot() {
     faCheck,
     faCheckSquare,
     faChild,
+    faCircle,
+    faClock,
     faClone,
+    faColumns,
     faCube,
+
+    /* D */
+    faDotCircle,
+
+    /* E */
+    faEllipsisH,
     faEraser,
+    faExclamationTriangle,
     faEye,
+    faEyeSlash,
+
+    /* F */
     faFemale,
     faFileAlt,
     faFlask,
+
+    /* H */
+    faHashtag,
+
+    /* I */
     faIndustry,
+
+    /* L */
     faLanguage,
+    faLink,
+
+    /* M */
     faMale,
+    faMapMarkerAlt,
     faMinus,
     faMinusSquare,
+    faMoneyBillAlt,
+
+    /* P */
     faPencil,
     faPlus,
     faPlusSquare,
+
+    /* R */
     faRocket,
+
+    /* S */
     faSearch,
     faSignInAlt,
     faSmile,
     faSpinner,
     faSquare,
+
+    /* T */
     faTimes,
     faTint,
     faToggleOff,
     faTrain,
+    faTrash,
     faTruck,
     faTv,
+
+    /* U */
     faUndo,
+    faUnlink,
     faUser,
     faUserSecret
   );
