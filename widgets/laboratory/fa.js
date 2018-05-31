92093754afb41a0bb275c0fb63d3b582209a50ec
Mathieu Schroeter <mathieu@schroetersa.ch>
2018-05-31 12:53:18 +0200

    Import more icons

--- widgets/laboratory/fa.js
+++ widgets/laboratory/fa.js
@@ -1,47 +1,97 @@
 import fontawesome from '@fortawesome/fontawesome';
 
 // TODO: add support for free version (for the open-source version of westeros)
+import faAngleDown from '@fortawesome/fontawesome-pro-solid/faAngleDown';
+import faAngleUp from '@fortawesome/fontawesome-pro-solid/faAngleUp';
+import faArchive from '@fortawesome/fontawesome-pro-solid/faArchive';
+import faArrowRight from '@fortawesome/fontawesome-pro-solid/faArrowRight';
+import faArrowAltCircleLeft from '@fortawesome/fontawesome-pro-solid/faArrowAltCircleLeft';
+import faBell from '@fortawesome/fontawesome-pro-solid/faBell';
 import faBicycle from '@fortawesome/fontawesome-pro-solid/faBicycle';
 import faCalendarPlus from '@fortawesome/fontawesome-pro-solid/faCalendarPlus';
 import faCar from '@fortawesome/fontawesome-pro-solid/faCar';
 import faCaretDown from '@fortawesome/fontawesome-pro-solid/faCaretDown';
+import faCartPlus from '@fortawesome/fontawesome-pro-solid/faCartPlus';
+import faCheck from '@fortawesome/fontawesome-pro-solid/faCheck';
+import faCheckSquare from '@fortawesome/fontawesome-pro-solid/faCheckSquare';
+import faChild from '@fortawesome/fontawesome-pro-solid/faChild';
+import faClone from '@fortawesome/fontawesome-pro-solid/faClone';
 import faCube from '@fortawesome/fontawesome-pro-light/faCube';
+import faEraser from '@fortawesome/fontawesome-pro-solid/faEraser';
+import faEye from '@fortawesome/fontawesome-pro-solid/faEye';
+import faFemale from '@fortawesome/fontawesome-pro-solid/faFemale';
 import faFileAlt from '@fortawesome/fontawesome-pro-solid/faFileAlt';
 import faFlask from '@fortawesome/fontawesome-pro-solid/faFlask';
+import faIndustry from '@fortawesome/fontawesome-pro-solid/faIndustry';
 import faLanguage from '@fortawesome/fontawesome-pro-solid/faLanguage';
+import faMale from '@fortawesome/fontawesome-pro-solid/faMale';
 import faMinus from '@fortawesome/fontawesome-pro-solid/faMinus';
+import faMinusSquare from '@fortawesome/fontawesome-pro-solid/faMinusSquare';
+import faPencil from '@fortawesome/fontawesome-pro-solid/faPencil';
 import faPlus from '@fortawesome/fontawesome-pro-solid/faPlus';
+import faPlusSquare from '@fortawesome/fontawesome-pro-solid/faPlusSquare';
 import faRocket from '@fortawesome/fontawesome-pro-solid/faRocket';
 import faSearch from '@fortawesome/fontawesome-pro-solid/faSearch';
 import faSignInAlt from '@fortawesome/fontawesome-pro-solid/faSignInAlt';
+import faSmile from '@fortawesome/fontawesome-pro-solid/faSmile';
 import faSpinner from '@fortawesome/fontawesome-pro-solid/faSpinner';
+import faSquare from '@fortawesome/fontawesome-pro-regular/faSquare';
+import faTimes from '@fortawesome/fontawesome-pro-solid/faTimes';
 import faTint from '@fortawesome/fontawesome-pro-solid/faTint';
+import faToggleOff from '@fortawesome/fontawesome-pro-light/faToggleOff';
 import faTrain from '@fortawesome/fontawesome-pro-solid/faTrain';
 import faTruck from '@fortawesome/fontawesome-pro-solid/faTruck';
 import faTv from '@fortawesome/fontawesome-pro-solid/faTv';
+import faUndo from '@fortawesome/fontawesome-pro-solid/faUndo';
 import faUser from '@fortawesome/fontawesome-pro-solid/faUser';
+import faUserSecret from '@fortawesome/fontawesome-pro-solid/faUserSecret';
 
 function boot() {
   fontawesome.library.add(
+    faAngleDown,
+    faAngleUp,
+    faArchive,
+    faArrowRight,
+    faArrowAltCircleLeft,
+    faBell,
     faBicycle,
     faCalendarPlus,
     faCar,
     faCaretDown,
+    faCartPlus,
+    faCheck,
+    faCheckSquare,
+    faChild,
+    faClone,
     faCube,
+    faEraser,
+    faEye,
+    faFemale,
     faFileAlt,
     faFlask,
+    faIndustry,
     faLanguage,
+    faMale,
     faMinus,
+    faMinusSquare,
+    faPencil,
     faPlus,
+    faPlusSquare,
     faRocket,
     faSearch,
     faSignInAlt,
+    faSmile,
     faSpinner,
+    faSquare,
+    faTimes,
     faTint,
+    faToggleOff,
     faTrain,
     faTruck,
     faTv,
-    faUser
+    faUndo,
+    faUser,
+    faUserSecret
   );
 }
 
