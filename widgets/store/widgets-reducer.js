fcc64fb5a1f8717b826d340ee53c1db0c906369f
Samuel Loup <samuel.loup@gmail.com>
2018-08-07 16:59:22 +0200

    Impl. widget reducer garbage collector

--- widgets/store/widgets-reducer.js
+++ widgets/store/widgets-reducer.js
@@ -1,9 +1,31 @@
 import {fromJS} from 'immutable';
+import transit from 'transit-immutable-js';
 import importer from '../importer/';
 
 const reducerImporter = importer('reducer');
 
 export default (state = fromJS({}), action = {}) => {
+  if (action.type === 'NEW_BACKEND_STATE') {
+    if (!action.data) {
+      return state;
+    }
+    const newState = transit.fromJSON(action.data);
+    if (newState.get('_xcraftPatch')) {
+      newState
+        .get('state')
+        .filter(
+          op =>
+            op.get('op') === 'remove' && state.has(op.get('path').substring(1))
+        )
+        .forEach(op => {
+          const pathToRemove = op.get('path').substring(1);
+          state = state.delete(pathToRemove);
+        });
+
+      return state;
+    }
+  }
+
   if (!action.type.startsWith('@widgets_')) {
     return state;
   }
