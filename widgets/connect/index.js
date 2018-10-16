217fd7e67ce83ade9bc493f5c96ce1338734b69b
Jonny Quarta <quarta@epsitec.ch>
2018-10-16 17:20:38 +0200

    Optimize connect component when parent is rendered

--- widgets/connect/index.js
+++ widgets/connect/index.js
@@ -3,65 +3,32 @@ import Widget from 'laboratory/widget';
 
 class ConnectedElement extends Widget {
   render() {
-    const {children} = this.props;
-    const childProps = {};
-
-    // Forward props to children (real component that has to be connected)
-    Object.keys(this.props).map(key => {
-      if (key !== 'children') {
-        if (key === 'key') {
-          childProps[key] = this.props[key] ? this.props[key] + '_item' : null;
-        } else {
-          childProps[key] = this.props[key];
-        }
-      }
-    });
-
+    const {children, ...childProps} = this.props;
     return <children.type {...children.props} {...childProps} />;
   }
 }
 
-class Connect extends Widget {
-  render() {
-    const {id, key, children} = this.props;
-    const self = this;
+function constructConnection(state, props) {
+  const connection = {};
 
-    function constructConnection(state) {
-      const connection = {};
-
-      Object.keys(self.props).map(key => {
-        if (
-          key !== 'children' &&
-          key !== 'key' &&
-          key !== 'do' &&
-          key !== 'id'
-        ) {
-          try {
-            if (typeof self.props[key] === 'function') {
-              connection[key] = self.props[key](state);
-            } else {
-              console.error('connected key ' + key + ' is not a function');
-            }
-          } catch (err) {
-            console.error('error connecting key ' + key + ': ');
-            console.error(err);
-          }
+  Object.keys(props).forEach(key => {
+    if (key !== 'children' && key !== 'key') {
+      try {
+        if (typeof props[key] === 'function') {
+          connection[key] = props[key](state);
+        } else {
+          console.error('Connect: connected key ' + key + ' is not a function');
         }
-      });
-
-      return connection;
+      } catch (err) {
+        console.error('Connect: error connecting key ' + key + ': ');
+        console.error(err);
+      }
     }
+  });
 
-    const Element = Widget.connect(state => constructConnection(state))(
-      ConnectedElement
-    );
-
-    return (
-      <Element do={self.props.do} id={id} key={key ? key + '_connected' : null}>
-        {children}
-      </Element>
-    );
-  }
+  return connection;
 }
+
+const Connect = Widget.connect(constructConnection)(ConnectedElement);
 
 export default Connect;
