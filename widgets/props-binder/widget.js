9a116933c1794d2a5619dc6942afc4b4001dbdec
Yannick Vessaz <vessaz@epsitec.ch>
2019-05-28 12:30:03 +0200

    Do not add "dispatch" to the props of the sub-component (and prettier)

--- widgets/props-binder/widget.js
+++ widgets/props-binder/widget.js
@@ -1,29 +1,32 @@
 import Widget from 'laboratory/widget';
 
-export default Widget.connect((state, props) => {
-  const {model, ...otherProps} = props;
-  return Object.entries(otherProps).reduce((properties, [key, path]) => {
-    if (key === 'value') {
-      properties[key] = path;
+export default Widget.connect(
+  (state, props) => {
+    const {model, ...otherProps} = props;
+    return Object.entries(otherProps).reduce((properties, [key, path]) => {
+      if (key === 'value') {
+        properties[key] = path;
+        return properties;
+      }
+
+      if (typeof path !== 'string') {
+        properties[key] = path;
+        return properties;
+      }
+
+      if (path.startsWith('$')) {
+        properties[key] = path.substring(1);
+        return properties;
+      }
+
+      //do binding
+      if (path.startsWith('.')) {
+        path = `${model}${path}`;
+      }
+
+      properties[key] = state.get(`newForms.${path}.value`);
       return properties;
-    }
-
-    if (typeof path !== 'string') {
-      properties[key] = path;
-      return properties;
-    }
-
-    if (path.startsWith('$')) {
-      properties[key] = path.substring(1);
-      return properties;
-    }
-
-    //do binding
-    if (path.startsWith('.')) {
-      path = `${model}${path}`;
-    }
-
-    properties[key] = state.get(`newForms.${path}.value`);
-    return properties;
-  }, {});
-});
+    }, {});
+  },
+  () => ({}) // Do not add "dispatch" to the props
+);
