187e194e8915d14b0469488ce0839633dcfb4c40
Yannick Vessaz <vessaz@epsitec.ch>
2019-05-28 14:07:07 +0200

    Move field-binder logic outside connect when it is possible

--- widgets/field-binder/widget.js
+++ widgets/field-binder/widget.js
@@ -1,85 +1,91 @@
+import React from 'react';
 import Widget from 'laboratory/widget';
 import Shredder from 'xcraft-core-shredder';
 
-export default Widget.connect(
-  (state, props) => {
-    const {value, format, model} = props;
-    if (!value) {
-      throw new Error('No value props provided, field-binder failed');
-    }
-    //do binding
-    let finalValue = '';
-    let path = value;
-    if (value.startsWith('.')) {
-      path = `${model}${value}`;
-    }
-
-    const fieldState = state.get(`newForms.${path}`);
-    if (fieldState) {
-      const edit = fieldState.get('edit');
-      if (edit) {
-        const raw = fieldState.get('raw');
-        finalValue = raw;
-      } else {
-        let value = fieldState.get('value');
-        if (format) {
-          value = format(value);
-        }
-        finalValue = value;
-      }
-    }
-
-    return {value: finalValue};
-  },
-  (dispatch, props) => {
-    const {parse, format, model} = props;
-    let path = props.value;
-
-    if (!path) {
-      return {};
-    }
-
-    if (path.startsWith('.')) {
-      path = `${model}${path}`;
-    }
-
-    return {
-      onFocus: () => {
-        dispatch((d, getState) => {
-          const fieldState = new Shredder(getState().newForms).get(path);
-          let val = '';
-          if (fieldState) {
-            val = fieldState.get('value');
-          }
-          d({
-            type: 'FIELD-FOCUS',
-            path,
-            value: format ? format(val) : val,
-          });
-        });
-      },
-      onBlur: e => {
-        dispatch({
-          type: 'FIELD-BLUR',
-          path,
-        });
-        let res = e.target.value;
-        if (parse) {
-          res = parse(e.target.value);
-        }
-
-        dispatch({
-          type: 'FIELD-CHANGED',
-          path,
-          value: res,
-        });
-      },
-      onChange: e =>
-        dispatch({
-          type: 'FIELD-RAW-CHANGED',
-          path,
-          value: e.target.value,
-        }),
-    };
+const beforeConnect = ConnectedComponent => props => {
+  const {value, model} = props;
+  if (!value) {
+    throw new Error(
+      'No "value" prop provided. You must add a prop "value" to the component connected with "field-binder".'
+    );
   }
-);
+  let newModel = value;
+  if (value.startsWith('.')) {
+    newModel = `${model}${value}`;
+  }
+  return <ConnectedComponent {...props} model={newModel} />;
+};
+
+const afterConnect = Component => props => {
+  const {format, parse, _useFormat, ...other} = props;
+  let {value} = props;
+  if (_useFormat && format) {
+    value = format(value);
+  }
+  return <Component {...other} value={value} />;
+};
+
+export default Component => {
+  const ConnectedComponent = Widget.connect(
+    (state, props) => {
+      const {model} = props;
+
+      const fieldState = state.get(`newForms.${model}`);
+      if (fieldState) {
+        const edit = fieldState.get('edit');
+        if (edit) {
+          return {value: fieldState.get('raw'), _useFormat: false};
+        }
+        return {value: fieldState.get('value'), _useFormat: true};
+      }
+
+      return {value: '', _useFormat: false}; // FIXME: handle 'model' which starts with 'backend' or 'widgets'
+    },
+    (dispatch, props) => {
+      const {parse, format, model} = props;
+
+      return {
+        onFocus: () => {
+          dispatch((d, getState) => {
+            const state = new Shredder(getState().newForms);
+            const fieldState = state.get(model);
+            let value = '';
+            if (fieldState) {
+              value = fieldState.get('value');
+            }
+            d({
+              type: 'FIELD-FOCUS',
+              path: model,
+              value: format ? format(value) : value,
+            });
+          });
+        },
+        onBlur: e => {
+          dispatch({
+            type: 'FIELD-BLUR',
+            path: model,
+          });
+
+          let value = e.target.value;
+          if (parse) {
+            value = parse(value);
+          }
+
+          dispatch({
+            type: 'FIELD-CHANGED',
+            path: model,
+            value,
+          });
+        },
+        onChange: e =>
+          dispatch({
+            type: 'FIELD-RAW-CHANGED',
+            path: model,
+            value: e.target.value,
+          }),
+      };
+    }
+  )(afterConnect(Component));
+
+  return beforeConnect(ConnectedComponent);
+};
