6bc7ccfa75df1df9e9584f3ca3048fce79e8191f
Yannick Vessaz <vessaz@epsitec.ch>
2019-07-04 17:08:39 +0200

    Wait the state is initialised before rendering children in frontend-form

--- widgets/frontend-form/widget.js
+++ widgets/frontend-form/widget.js
@@ -2,6 +2,24 @@ import React from 'react';
 import Widget from 'laboratory/widget';
 import WithModel from '../with-model/widget';
 
+class FrontendFormWaitLoadedNC extends Widget {
+  render() {
+    if (!this.props.loaded) {
+      return null;
+    }
+    return this.props.children;
+  }
+}
+
+const FrontendFormWaitLoaded = Widget.connect((state, props) => {
+  if (state.get(props.path) !== undefined) {
+    return {
+      loaded: true,
+    };
+  }
+  return {};
+})(FrontendFormWaitLoadedNC);
+
 export default class FrontendForm extends Widget {
   constructor() {
     super(...arguments);
@@ -9,10 +27,12 @@ export default class FrontendForm extends Widget {
   }
 
   render() {
-    // TODO: should not render when the state is not initialised
+    const path = `widgets.${this.widgetId}`;
     return (
-      <WithModel model={`widgets.${this.widgetId}`}>
-        {this.props.children}
+      <WithModel model={path}>
+        <FrontendFormWaitLoaded path={path}>
+          {this.props.children}
+        </FrontendFormWaitLoaded>
       </WithModel>
     );
   }
