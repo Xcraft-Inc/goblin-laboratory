88f80c56debc8a87c369bb7f6fd4eda067eab4b6
Yannick Vessaz <vessaz@epsitec.ch>
2019-11-29 15:26:33 +0100

    Change importer to be able to create custom importers more easily

---
+++ widgets/importer/importer.js
@@ -0,0 +1,45 @@
+import path from 'path';
+
+export default function importer(config) {
+  const cache = {};
+
+  const importAll = (kind, r) => {
+    cache[kind] = {};
+
+    if (r) {
+      const files = r.keys();
+      files.forEach(file => {
+        const nameSpace =
+          kind === 'theme-context'
+            ? file.replace(/.*[\\/.]goblin-([^\\/]+).*/, '$1')
+            : path.basename(path.dirname(file));
+        cache[kind][nameSpace] = r(file);
+      });
+    }
+  };
+
+  const getter = kind => (name, key) => {
+    if (!cache[kind][name]) {
+      return null;
+    }
+    if (key) {
+      return cache[kind][name][key];
+    }
+    return cache[kind][name].default;
+  };
+
+  return kind => {
+    if (cache[kind]) {
+      return getter(kind);
+    }
+
+    if (!(kind in config)) {
+      throw new Error(`Unsupported kind: ${kind} for importer`);
+    }
+
+    const r = config[kind];
+    importAll(kind, r);
+
+    return getter(kind);
+  };
+}
--- widgets/importer/index.js
+++ widgets/importer/index.js
@@ -1,100 +1,16 @@
-//T:2019-02-27
+import importer from './importer.js';
 
-import path from 'path';
-
-const cache = {};
-
-const importAll = (kind, r) => {
-  const files = r.keys();
-
-  cache[kind] = {};
-
-  files.forEach(file => {
-    const nameSpace =
-      kind === 'theme-context'
-        ? file.replace(/.*[\\/.]goblin-([^\\/]+).*/, '$1')
-        : path.basename(path.dirname(file));
-    cache[kind][nameSpace] = r(file);
-  });
+// prettier-ignore
+const defaultConfig = {
+  'tasks': require.context('../../../', true, /\/widgets\/[^/]+\/tasks\.js$/),
+  'view': require.context('../../../', true, /\/widgets\/[^/]+\/view\.js$/),
+  'partial': require.context('../../../', true, /\/widgets\/[^/]+\/partial\.js$/),
+  'styles': require.context('../../../', true, /\/widgets\/[^/]+\/styles\.js$/),
+  'widget': require.context('../../../', true, /\/widgets\/[^/]+\/widget\.js$/),
+  'ui': require.context('../../../', true, /\/widgets\/[^/]+\/ui\.js$/),
+  'reducer': require.context('../../../', true, /\/widgets\/[^/]+\/reducer\.js$/),
+  'compensator': require.context('../../../', true, /\/widgets\/[^/]+\/compensator\.js$/),
+  'theme-context': require.context('../../..', true, /\/widgets\/theme-context\/index\.js$/),
 };
 
-const getter = kind => (name, key) => {
-  if (!cache[kind][name]) {
-    return null;
-  }
-  if (key) {
-    return cache[kind][name][key];
-  }
-  return cache[kind][name].default;
-};
-
-export default kind => {
-  if (cache[kind]) {
-    return getter(kind);
-  }
-
-  switch (kind) {
-    case 'tasks':
-      importAll(
-        kind,
-        require.context('../../../', true, /\/widgets\/[^/]+\/tasks\.js$/)
-      );
-      break;
-    case 'view':
-      importAll(
-        kind,
-        require.context('../../../', true, /\/widgets\/[^/]+\/view\.js$/)
-      );
-      break;
-    case 'partial':
-      importAll(
-        kind,
-        require.context('../../../', true, /\/widgets\/[^/]+\/partial\.js$/)
-      );
-      break;
-    case 'styles':
-      importAll(
-        kind,
-        require.context('../../../', true, /\/widgets\/[^/]+\/styles\.js$/)
-      );
-      break;
-    case 'widget':
-      importAll(
-        kind,
-        require.context('../../../', true, /\/widgets\/[^/]+\/widget\.js$/)
-      );
-      break;
-    case 'ui':
-      importAll(
-        kind,
-        require.context('../../../', true, /\/widgets\/[^/]+\/ui\.js$/)
-      );
-      break;
-    case 'reducer':
-      importAll(
-        kind,
-        require.context('../../../', true, /\/widgets\/[^/]+\/reducer\.js$/)
-      );
-      break;
-    case 'compensator':
-      importAll(
-        kind,
-        require.context('../../../', true, /\/widgets\/[^/]+\/compensator\.js$/)
-      );
-      break;
-    case 'theme-context':
-      importAll(
-        kind,
-        require.context(
-          '../../..',
-          true,
-          /\/widgets\/theme-context\/index\.js$/
-        )
-      );
-      break;
-    default:
-      throw new Error(`Unsupported kind: ${kind} for importer`);
-  }
-
-  return getter(kind);
-};
+export default importer(defaultConfig);
