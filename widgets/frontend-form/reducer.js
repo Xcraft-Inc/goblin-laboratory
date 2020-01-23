078778b7645b83395229ef674cf91ccd59b2c5c0
Samuel Loup <samuel.loup@gmail.com>
2020-01-23 19:24:46 +0100

    Frontend-form: don't reINIT the state if already setted

--- widgets/frontend-form/reducer.js
+++ widgets/frontend-form/reducer.js
@@ -5,7 +5,10 @@ const initialState = new Shredder({});
 export default (state = initialState, action = {}) => {
   switch (action.type) {
     case 'INIT': {
-      return state.set('', action.initialState);
+      if (state.get('') === undefined) {
+        return state.set('', action.initialState);
+      }
+      return state;
     }
     case 'CHANGE': {
       return state.set(action.path, action.newValue);
