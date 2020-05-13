471ea0764c48a113e5fe9b2ae8c24c733d7fa1a2
Samuel Loup <samuel.loup@gmail.com>
2020-05-13 23:00:54 +0200

    nav: remove now useless complicated logic, all is now handled with currentLocation nav ctx

--- widgets/store/root-reducer.js
+++ widgets/store/root-reducer.js
@@ -17,47 +17,10 @@ const initialState = new Shredder({
 
 export function routerReducer(state = initialState, {type, payload} = {}) {
   if (type === LOCATION_CHANGE) {
-    const location = state.get('location');
-    if (location) {
-      //This complexity:
-      //Prevent loosing hash when a nav is initiated in front-end and erased by a backend nav
-      const currentLoc = location.get('pathname');
-      const currentSearch = location.get('search');
-      const currentHash = location.get('hash');
-
-      if (currentHash !== '' && payload.location.hash !== currentHash) {
-        return state.merge({
-          location: payload.location,
-          action: payload.action,
-        });
-      }
-
-      if (
-        currentLoc.startsWith(payload.location.pathname) &&
-        (payload.location.search.startsWith(currentSearch) ||
-          currentSearch.startsWith(payload.location.search))
-      ) {
-        // pathname looks similar, we can investigate
-        const currentPath = currentLoc.split('/');
-        const newPath = payload.location.pathname.split('/');
-        const cPl = currentPath.length - 1;
-        const nPl = newPath.length - 1;
-        if (cPl > nPl) {
-          //Current path contains more info (for ex. hinter) in this case, we must skip the nav
-          return state.set(
-            'location',
-            state.get('location').set('search', payload.location.search)
-          );
-        } else if (cPl === nPl && currentPath[cPl] === newPath[nPl]) {
-          //Same path, but same value?
-          return state.set(
-            'location',
-            state.get('location').set('search', payload.location.search)
-          );
-        }
-      }
-    }
-    return state.merge({location: payload.location, action: payload.action});
+    return state.merge({
+      location: payload.location,
+      action: payload.action,
+    });
   }
 
   return state;
