f84d5ce30e5f94fb207690ef879179b4fb72badf
Mathieu Schroeter <mathieu@schroetersa.ch>
2020-06-30 16:26:40 +0200

    Compare NODE_ENV only with development
    It's possible that NODE_ENV is not set with production run.

--- widgets/store/store.js
+++ widgets/store/store.js
@@ -12,7 +12,7 @@ export default function configureStore(initialState, history, send) {
   const routerHistory = routerMiddleware(history);
 
   const composeEnhancers =
-    (process.env.NODE_ENV !== 'production' &&
+    (process.env.NODE_ENV === 'development' &&
       window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
     compose;
 
