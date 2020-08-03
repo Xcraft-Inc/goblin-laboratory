a23af4921ce9dff7da38735113c444ff38493aeb
Samuel Loup <samuel.loup@gmail.com>
2020-08-03 12:07:44 +0200

    Frontend-form, remove not working correctly feature
    Initialstate loading at constuction is not working correctly
    in some case, the widgets state in not ready or collected,
    and the expected behavior is conduct in nothing displayed...
    
    We must find another way to initialize a pure frontend form
    with an initialstate via props.

--- widgets/frontend-form/widget.js
+++ widgets/frontend-form/widget.js
@@ -2,38 +2,14 @@ import React from 'react';
 import Widget from 'goblin-laboratory/widgets/widget';
 import WithModel from '../with-model/widget';
 
-class FrontendFormWaitLoadedNC extends Widget {
-  render() {
-    if (!this.props.loaded) {
-      return null;
-    }
-    return this.props.children;
-  }
-}
-
-const FrontendFormWaitLoaded = Widget.connect((state, props) => {
-  if (state.get(props.path) !== undefined) {
-    return {
-      loaded: true,
-    };
-  }
-  return {};
-})(FrontendFormWaitLoadedNC);
-
 export default class FrontendForm extends Widget {
   constructor() {
     super(...arguments);
-    this.dispatch({type: 'INIT', initialState: this.props.initialState});
   }
 
   render() {
-    const path = `widgets.${this.widgetId}`;
-    return (
-      <WithModel model={path}>
-        <FrontendFormWaitLoaded path={path}>
-          {this.props.children}
-        </FrontendFormWaitLoaded>
-      </WithModel>
-    );
+    const path = `widgets.${this.props.widgetId}`;
+
+    return <WithModel model={path}>{this.props.children}</WithModel>;
   }
 }
