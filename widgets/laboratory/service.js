630d7ac0a50ca63bce618fad5d35bdad81508bef
Samuel Loup <loup@epsitec.ch>
2021-09-08 11:07:33 +0200

    restore code in this branch

--- widgets/laboratory/service.js
+++ widgets/laboratory/service.js
@@ -344,7 +344,7 @@ Goblin.registerQuest(goblinName, 'listen', function* (
 
   if (userId) {
     const wmAPI = quest.getAPI(`wm@${quest.goblin.id}`);
-    // concerns wip/shield: yield wmAPI.setUserId({userId});
+    yield wmAPI.setUserId({userId});
   }
 
   const labId = quest.goblin.id;
