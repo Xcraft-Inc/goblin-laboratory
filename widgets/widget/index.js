5b78a2fd2fdecab43e9502bd05a2a09f58a0f315
Yannick Vessaz <vessaz@epsitec.ch>
2021-10-27 13:59:23 +0200

    Always get the user session from the backend state

--- widgets/widget/index.js
+++ widgets/widget/index.js
@@ -1160,14 +1160,9 @@ class Widget extends React.Component {
   }
 
   static getUserSession(state) {
-    if (window.isBrowser) {
-      //TODO: impl. browser session
-      return new Shredder({locale: navigator.language});
-    } else {
-      return state.get(
-        `backend.${state.get(`backend.${window.labId}.clientSessionId`)}`
-      );
-    }
+    return state.get(
+      `backend.${state.get(`backend.${window.labId}.clientSessionId`)}`
+    );
   }
 
   static getLoginSession(state) {
