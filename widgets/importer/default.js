92009a60896d0bdd5b4aa475a494f9eeabf1988f
Mathieu Schroeter <mathieu@schroetersa.ch>
2022-10-18 11:16:01 +0200

    Use an external file for the importer definition
    And use node_modules in order to be sure that the importer
    works when goblin-laboratory is inside lib/ or only in
    node_modules/.

---
+++ widgets/importer/default.js
@@ -0,0 +1,14 @@
+// prettier-ignore
+const defaultConfig = {
+  'tasks':         require.context('../../../../node_modules/', true, /\/widgets\/[^/]+\/tasks\.js$/),
+  'view':          require.context('../../../../node_modules/', true, /\/widgets\/[^/]+\/view\.js$/),
+  'partial':       require.context('../../../../node_modules/', true, /\/widgets\/[^/]+\/partial\.js$/),
+  'styles':        require.context('../../../../node_modules/', true, /\/widgets\/[^/]+\/styles\.js$/),
+  'widget':        require.context('../../../../node_modules/', true, /\/widgets\/[^/]+\/widget\.js$/),
+  'ui':            require.context('../../../../node_modules/', true, /\/widgets\/[^/]+\/ui\.js$/),
+  'reducer':       require.context('../../../../node_modules/', true, /\/widgets\/[^/]+\/reducer\.js$/),
+  'compensator':   require.context('../../../../node_modules/', true, /\/widgets\/[^/]+\/compensator\.js$/),
+  'theme-context': require.context('../../../../node_modules/', true, /\/widgets\/theme-context\/index\.js$/),
+};
+
+export default defaultConfig;
--- widgets/importer/index.js
+++ widgets/importer/index.js
@@ -1,16 +1,4 @@
 import importer from './importer.js';
-
-// prettier-ignore
-const defaultConfig = {
-  'tasks': require.context('../../../', true, /\/widgets\/[^/]+\/tasks\.js$/),
-  'view': require.context('../../../', true, /\/widgets\/[^/]+\/view\.js$/),
-  'partial': require.context('../../../', true, /\/widgets\/[^/]+\/partial\.js$/),
-  'styles': require.context('../../../', true, /\/widgets\/[^/]+\/styles\.js$/),
-  'widget': require.context('../../../', true, /\/widgets\/[^/]+\/widget\.js$/),
-  'ui': require.context('../../../', true, /\/widgets\/[^/]+\/ui\.js$/),
-  'reducer': require.context('../../../', true, /\/widgets\/[^/]+\/reducer\.js$/),
-  'compensator': require.context('../../../', true, /\/widgets\/[^/]+\/compensator\.js$/),
-  'theme-context': require.context('../../..', true, /\/widgets\/theme-context\/index\.js$/),
-};
+import defaultConfig from './default.js';
 
 export default importer(defaultConfig);
