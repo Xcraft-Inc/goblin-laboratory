7d38013834ed0df8c505f51e88e97e3fd40d3d8a
Mathieu Schroeter <mathieu@schroetersa.ch>
2025-10-30 16:21:05 +0100

    Add a test for up/down in the history

--- test/termux.spec.js
+++ test/termux.spec.js
@@ -1,11 +1,11 @@
 'use strict';
 
 const {expect} = require('chai');
+const path = require('node:path');
 const {Elf} = require('xcraft-core-goblin/lib/test.js');
+const {TermuxLogic, Termux} = require('../lib/termux.js');
 
-describe('goblin.laboratory.termux', function () {
-  const {TermuxLogic} = require('../lib/termux.js');
-
+describe('goblin.laboratory.termux.command', function () {
   it('history', function () {
     const termuxLogic = Elf.trial(TermuxLogic);
 
@@ -20,5 +20,61 @@ describe('goblin.laboratory.termux', function () {
     expect(termuxLogic.state.history.length).to.be.equal(4);
     expect(termuxLogic.state.history[2]).to.be.equal('$ cmd2 ');
     expect(termuxLogic.state.history[3]).to.be.equal('done 2');
+  });
+});
+
+describe('goblin.laboratory.termux.completion', function () {
+  let runner;
+
+  this.beforeAll(function () {
+    runner = new Elf.Runner();
+    runner.init();
+  });
+
+  this.afterAll(function () {
+    runner.dispose();
+  });
+
+  it('history', async function () {
+    this.timeout(process.env.NODE_ENV === 'development' ? 1000000 : 40000);
+
+    /** @this {Elf} */
+    async function test() {
+      const xBus = require('xcraft-core-bus');
+      await xBus.loadModule(
+        this.quest.resp,
+        ['termux.js'],
+        path.join(__dirname, '..'),
+        {}
+      );
+
+      let state;
+      const termux = new Termux(this);
+
+      await termux.init();
+
+      await termux.beginCommand('$', 'cmd1', []);
+      await termux.endCommand('done 1');
+      await termux.beginCommand('$', 'cmd2', []);
+      await termux.endCommand('done 2');
+
+      await termux.setFromHistory(true, 'cmd2 '); // UP
+      state = await this.quest.getState('termux');
+      expect(state.get('completion')).to.be.equal('cmd1');
+
+      await termux.setFromHistory(true, 'cmd1 '); // UP
+      state = await this.quest.getState('termux');
+      expect(state.get('completion')).to.be.equal('cmd1');
+
+      await termux.setFromHistory(false, 'cmd1 '); // DOWN
+      state = await this.quest.getState('termux');
+      expect(state.get('completion')).to.be.equal('cmd2');
+
+      await termux.setFromHistory(false, 'cmd2 '); // DOWN
+      state = await this.quest.getState('termux');
+      expect(state.get('completion')).to.be.equal('<empty>');
+    }
+
+    await runner.it(test);
   });
 });
