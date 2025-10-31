e9a6a0cfd69e3b551e1fe6adf476ac10cd8f301c
Mathieu Schroeter <mathieu@schroetersa.ch>
2025-10-31 16:35:49 +0100

    Fix tests

--- test/termux.spec.js
+++ test/termux.spec.js
@@ -12,13 +12,13 @@ describe('goblin.laboratory.termux.command', function () {
     termuxLogic.beginCommand('$', 'cmd1', []);
     termuxLogic.endCommand('done 1');
     expect(termuxLogic.state.history.length).to.be.equal(2);
-    expect(termuxLogic.state.history[0]).to.be.equal('$ cmd1 ');
+    expect(termuxLogic.state.history[0]).to.be.equal('$ cmd1');
     expect(termuxLogic.state.history[1]).to.be.equal('done 1');
 
     termuxLogic.beginCommand('$', 'cmd2', []);
     termuxLogic.endCommand('done 2');
     expect(termuxLogic.state.history.length).to.be.equal(4);
-    expect(termuxLogic.state.history[2]).to.be.equal('$ cmd2 ');
+    expect(termuxLogic.state.history[2]).to.be.equal('$ cmd2');
     expect(termuxLogic.state.history[3]).to.be.equal('done 2');
   });
 });
