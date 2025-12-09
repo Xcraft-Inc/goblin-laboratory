e9bc5fe384c0e38479d6c971b82674fad8ce7e12
Yannick Vessaz <vessaz@epsitec.ch>
2025-12-09 14:48:51 +0100

    Do not add a "dispatch" prop by default
    It's not usefull as the Widget class has already access to dispatch.
    It prevents warnings when the props are forwarded to a dom element.

--- widgets/widget/utils/connect.js
+++ widgets/widget/utils/connect.js
@@ -34,7 +34,7 @@ export default function (
 ) {
   return connect(
     withShredder(mapStateToProps),
-    mapDispatchToProps,
+    mapDispatchToProps || (() => ({})),
     mergeProps,
     {
       pure: true,
