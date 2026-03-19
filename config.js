98924f572aa960e7ad288f2917e669100dad316a
Mathieu Schroeter <mathieu@schroetersa.ch>
2026-03-19 13:59:16 +0100

    Remove dead zoom config

--- config.js
+++ config.js
@@ -3,11 +3,4 @@
 /**
  * Retrieve the inquirer definition for xcraft-core-etc
  */
-module.exports = [
-  {
-    type: 'input',
-    name: 'defaultZoom',
-    message: 'Set default zoom for Electron frontend',
-    default: 1.0,
-  },
-];
+module.exports = [];
