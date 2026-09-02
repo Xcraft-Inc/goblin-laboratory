9c48c06ea814173fe75e6d2f693d87de60f34a1f
Yannick Vessaz <vessaz@epsitec.ch>
2026-09-02 18:17:51 +0200

    Better merge inFunc and outFunc in map-c

--- widgets/connect-helpers/map-c.js
+++ widgets/connect-helpers/map-c.js
@@ -1,16 +1,51 @@
 import C, {ConnectedProp} from './c.js';
 
+function mergeInFuncs(inFunc1, inFunc2) {
+  if (inFunc1) {
+    if (inFunc2) {
+      return (...args) => inFunc2(inFunc1(...args));
+    }
+    return inFunc1;
+  }
+  return inFunc2;
+}
+
+function mergeOutFuncs(outFunc1, outFunc2, inFunc1) {
+  if (outFunc1) {
+    if (outFunc2) {
+      if (outFunc1.length > 1 || outFunc2.length > 1) {
+        if (inFunc1) {
+          return (newValue, oldValue, ...oldValues) =>
+            outFunc1(
+              outFunc2(newValue, inFunc1(oldValue, ...oldValues)),
+              oldValue,
+              ...oldValues
+            );
+        }
+        return (newValue, oldValue, ...oldValues) =>
+          outFunc1(
+            outFunc2(newValue, oldValue, ...oldValues),
+            oldValue,
+            ...oldValues
+          );
+      }
+      return (newValue) => outFunc1(outFunc2(newValue));
+    }
+    return outFunc1;
+  }
+  if (outFunc2 && outFunc2.length > 1 && inFunc1) {
+    return (newValue, oldValue, ...oldValues) =>
+      outFunc2(newValue, inFunc1(oldValue, ...oldValues));
+  }
+  return outFunc2;
+}
+
 export default function mapC(value, inFunc, outFunc) {
   if (value instanceof ConnectedProp) {
     return C(
       value.path,
-      value.inFunc ? (...args) => inFunc(value.inFunc(...args)) : inFunc,
-      value.outFunc
-        ? value.outFunc.length > 1 || outFunc.length > 1
-          ? (newValue, ...oldValues) =>
-              value.outFunc(outFunc(newValue, ...oldValues), ...oldValues)
-          : (newValue) => value.outFunc(outFunc(newValue))
-        : outFunc
+      mergeInFuncs(value.inFunc, inFunc),
+      mergeOutFuncs(value.outFunc, outFunc, value.inFunc)
     );
   }
   return inFunc(value);
